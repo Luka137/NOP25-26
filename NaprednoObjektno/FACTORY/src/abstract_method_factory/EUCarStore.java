41041fee49a9fce053b1d55f9d3b18c106504890
Luka137 <gobinluka7@gmail.com>
2025-12-03 17:32:32 +0100

    Commit

---
+++ NaprednoObjektno/FACTORY/src/abstract_method_factory/EUCarStore.java
@@ -0,0 +1,22 @@
+package abstract_method_factory;
+
+import pckg_simple_factory.Car;
+import pckg_simple_factory.CarType;
+
+public class EUCarStore extends CarStore{
+
+    @Override
+    public Car createCar(CarType carType, String model, String manufacturer) {
+        Car car = null;
+
+        if(carType.equals(CarType.SUV)){
+            car = new EUSUV(manufacturer, model);
+        }
+
+        if(carType.equals(CarType.SEDAN)){
+            car = new EUSedan(manufacturer, model);
+        }
+
+        return car;
+    }
+}
---
+++ NaprednoObjektno/FACTORY/src/pckg_simple_factory/CarStore.java
@@ -0,0 +1,14 @@
+package pckg_simple_factory;
+
+public class CarStore {
+
+    private String carStoreName;
+
+    public CarStore(String name){
+        this.carStoreName = name;
+    }
+
+    public Car orderCar(CarType carType, String model, String manufacturer){
+        return SimpleCarCreator.createCar(model,manufacturer,carType);
+    }
+}
